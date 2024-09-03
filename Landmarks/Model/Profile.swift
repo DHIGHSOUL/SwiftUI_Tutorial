1224e3e51ce1427015a23587b48bbf746e159630
DHGIGHSOUL <highsoul1227@gmail.com>
2024-09-03 16:39:34 +0900

    Chapter3_App design and layout

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by ROLF J. on 9/2/24.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "JKH")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "⛄️"
+        
+        var id: String { rawValue }
+    }
+}
